d33410343f62a6c1f38ba3bc5835a72b6abfe294
JacobRothDevelopment <rothy784715+github@gmail.com>
2023-10-10 01:55:55 -0400

    adding test examples

--- components/head.php
+++ components/head.php
@@ -6,6 +6,8 @@
         <?php echo $data->title ?? "Simple Framework"; ?>
     </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/style.css">
+    <script src="assets/script.js"></script>
 </head>
 
 <body>
